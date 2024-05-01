1a93a3128d0b9a4df21f7e48c6dae62a5b250b31
NicholasTheisen <nicholas.theisen1997@gmail.com>
2024-05-01 16:06:57 -0700

    added functionality

--- filters.rb
+++ filters.rb
@@ -3,14 +3,36 @@
 
 def find(id)
     # Your code Here
+    candidates.each do |candidate|
+      if candidate[:id] == id
+        return candidate
+      end
+    end
+    return nil
   end
   
   def experienced?(candidate)
     # Your code Here
+    if candidate[:years_of_experience] >= 2
+      return true
+    else
+      return false
+    end
   end
   
   def qualified_candidates(candidates)
     # Your code Here
+    qualified_candidates = []
+    candidates.each do |candidate|
+      if experienced?(candidate) && github_points?(candidate) && languages?(candidate) && date_applied?(candidate) && age?(candidate)
+        qualified_candidates << candidate
+      end
+    end
+    return qualified_candidates
   end
   
   # More methods will go below
+
+  def ordered_by_qualifications(candidates)
+    candidates.sort_by { |candidate| [candidate[:years_of_experience], candidate[:github_points]] }.reverse
+  end
